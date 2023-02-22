b8b966cda54ed19db1fdd97ee1ea3200c38d87c6
ahmedmadbouly186 <ahmedmadbouly186@gmail.com>
2023-02-22 17:55:02 +0200

    solve problems a-c-h-j-k-n-o sheet 2 BFS coach academy level3

--- coach_acdemy_shhet2_bfs.cpp
+++ coach_acdemy_shhet2_bfs.cpp
@@ -30,17 +30,28 @@ void prob_g(void);
 void prob_h(void);
 void prob_i(void);
 
-void BFS_j(vector<vector<char>>&maze, vector<pair<int, int>>&out, int startx, int starty);
+void BFS_j(vector<vector<char>>& maze, vector<pair<int, int>>& out, int startx, int starty);
 void prob_j(void);
-void prob_k(void);
-void prob_l(void);
 
+ll BFS_k(vector<vector<int>>& edges, vector<ll>& visit, int node, ll val);
+void prob_k(void);
+
+void prob_l(void);
+void prob_M(void);
+void prob_N(void);
+
+/// <summary>
+/// BFS code inside the func itself
+/// </summary>
+void prob_O(void);
+
+void prob_P(void);
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	prob_j();
+	prob_N();
 	return 0;
 }
 void BFS_a(vector<vector<int>>& edges, vector<int>& visit, int n, int start) {
@@ -74,11 +85,9 @@ void prob_a(void) {
 	ff(i, 1, n + 1) {
 		if (visit[i] != -1)cnt++;
 	}
-	if (cnt == n && m == n - 1)cout << "YES" << endl;
-	else cout << "NO" << endl;
+	if (cnt == n && m == n - 1)cout << "yes" << endl;
+	else cout << "no" << endl;
 }
-
-
 void prob_b(void) {
 
 }
@@ -109,6 +118,38 @@ void prob_c(void) {
 	cout << visit[m] << endl;
 }
 void prob_d(void) {
+	int n, m; cin >> n >> m;
+	vector<vector<int>>forest(n + 1, vector<int>(m + 1, -1));
+	int dy[4] = { 1,-1,0,0 };
+	int dx[4] = { 0,0,1,-1 };
+	int k; cin >> k;
+	queue<pair<int, int>>q;
+	f(i, k) {
+		int u, v; cin >> u >> v;
+		forest[u][v] = 0;
+		q.push({ u,v });
+	}
+	while (!q.empty())
+	{
+		int cury = q.front().first, curx = q.front().second;
+		f(i, 4)
+			if (cury + dy[i] >= 1 && cury + dy[i] <= n && curx + dx[i] >= 1 && curx + dx[i] <= m && forest[cury + dy[i]][curx + dx[i]] == -1)
+			{
+				forest[cury + dy[i]][curx + dx[i]] = forest[cury][curx] + 1;
+				q.push({ cury + dy[i],curx + dx[i] });
+			}
+		q.pop();
+	}
+	int maxi = -1;
+	int indexx = -1, indexy = -1;
+	ff(i, 1, n + 1)
+		ff(j, 1, 1 + m)
+		if (forest[i][j] > maxi) {
+			maxi = forest[i][j];
+			indexy = i;
+			indexx = j;
+		}
+	cout << indexy << " " << indexx << endl;
 
 }
 void prob_e(void) {
@@ -120,8 +161,37 @@ void prob_f(void) {
 void prob_g(void) {
 
 }
-void prob_h(void) {
 
+ll BFS_h(vector<vector<int>>& edges, vector<ll>& visit, int node, ll val) {
+	if (visit[node] != -1)return 0;
+	visit[node] = 0;
+	ll maxi1 = 0;
+	ll maxi2 = 0;
+	for (auto x : edges[node]) {
+		ll out = BFS_h(edges, visit, x, val + 1);
+		if (out > maxi1) {
+			maxi2 = maxi1; maxi1 = out;
+		}
+		else if (out > maxi2)maxi2 = out;
+	}
+	maxi2 = max(maxi2, val);
+	visit[node] = maxi1 + maxi2;
+	return maxi1 + 1;
+}
+void prob_h(void) {
+	int n, m; cin >> n >> m;
+	vector<vector<int>>edges(n + 1, vector<int>(0));
+	f(i, m) {
+		int u, v;
+		cin >> u >> v;
+		edges[u].push_back(v);
+		edges[v].push_back(u);
+	}
+	vector<ll> visit(n + 1, -1);
+	BFS_h(edges, visit, 1, 0);
+	ll out = -1;
+	ff(i, 1, n + 1)out = max(out, visit[i]);
+	cout << out << endl;
 }
 void prob_i(void) {
 
@@ -143,7 +213,7 @@ void BFS_j(vector<vector<char>>& maze, vector<pair<int, int>>& out, int startx, 
 		f(i, 4) {
 			int childx = curx + dx[i];
 			int childy = cury + dy[i];
-			if (childx >= 0 && childx < n && childy>=0 && childy < m && visit[childx][childy] == false && maze[childx][childy] == '.') {
+			if (childx >= 0 && childx < n && childy >= 0 && childy < m && visit[childx][childy] == false && maze[childx][childy] == '.') {
 				visit[childx][childy] = true;
 				q.push({ childx ,childy });
 			}
@@ -152,7 +222,7 @@ void BFS_j(vector<vector<char>>& maze, vector<pair<int, int>>& out, int startx, 
 		q.pop();
 		out.push_back({ curx,cury });
 	}
-	
+
 }
 void prob_j(void) {
 	int n, m, k;
@@ -185,9 +255,123 @@ void prob_j(void) {
 
 	}
 }
+
+ll BFS_k(vector<vector<int>>& edges, vector<ll>& visit, int node, ll val) {
+	if (visit[node] != -1)return 0;
+	visit[node] = 0;
+	ll maxi1 = 0;
+	ll maxi2 = 0;
+	for (auto x : edges[node]) {
+		ll out = BFS_k(edges, visit, x, val + 1);
+		if (out > maxi1) {
+			maxi2 = maxi1; maxi1 = out;
+		}
+		else if (out > maxi2)maxi2 = out;
+	}
+	maxi2 = max(maxi2, val);
+	visit[node] = maxi1 + maxi2;
+	return maxi1 + 1;
+}
 void prob_k(void) {
+	int n; cin >> n;
+	vector<vector<int>>edges(n + 1, vector<int>(0));
+	f(i, n - 1) {
+		int u, v;
+		cin >> u >> v;
+		edges[u].push_back(v);
+		edges[v].push_back(u);
+	}
+	vector<ll> visit(n + 1, -1);
+	BFS_k(edges, visit, 1, 0);
+	ll out = -1;
+	ff(i, 1, n + 1)out = max(out, visit[i]);
+	cout << out << endl;
+}
+
+void prob_l(void) {
 
 }
-void prob_l(void) {
+
+void prob_M(void) {
+
+}
+bool BFS_N_help(vector<vector<int>>& edges, vector<bool>& visit, map<pair<int, int>, bool>& m, vector<int>& out,int node,bool prob) {
+	if (visit[node])return false;
+	visit[node] = 1;
+	bool child = false;
+	for (auto x : edges[node]) {
+		child |= BFS_N_help(edges, visit, m, out, x, m[{min(x, node), max(x, node)}]);
+	}
+	if (prob && !child)out.push_back(node);
+	return child || prob;
+}
+void BFS_N(vector<vector<int>>& edges, vector<bool>& visit, map<pair<int, int>, bool>& m, vector<int>& out) {
+	visit[1] = 1;
+	for (auto x : edges[1]) {
+		BFS_N_help(edges, visit, m, out, x, m[{1, x}]);
+	}
+}
+void prob_N(void) {
+	int n; cin >> n;
+	vector<vector<int>>edges(n + 1, vector<int>(0));
+	vector<bool>visit(n + 1, 0);
+	map<pair<int, int>, bool>m;
+	vector<int>out;
+	f(i, n - 1) {
+		int u, v, t; cin >> u >> v >> t;
+		edges[u].push_back(v);
+		edges[v].push_back(u);
+		m[{min(u, v), max(u, v)}] = t == 2;
+	}
+	BFS_N(edges, visit, m, out);
+	int N = out.size();
+	cout << N << endl;
+	f(i, N) {
+		cout << out[i] << " ";
+	}
+	cout << endl;
+}
+
+void prob_O(void) {
+	int n; cin >> n;
+	vector<set<int>>edges(n + 1, set<int>());
+	f(i, n - 1) {
+		int u, v; cin >> u >> v;
+		edges[u].insert(v);
+		edges[v].insert(u);
+	}
+	vector<int>input(n);
+	f(i, n) {
+		cin >> input[i];
+	}
+
+	bool ac = input[0] == 1;
+	queue<int>q;
+	int cur = 1;
+	ff(i, 1, n) {
+		while (edges[cur].size() == 0)
+		{
+			if (q.empty()) {
+				ac = false;
+				break;
+			}
+			else {
+				cur = q.front();
+				q.pop();
+			}
+		}
+		if (ac) {
+			if (edges[cur].find(input[i]) != edges[cur].end()) {
+				q.push(input[i]);
+				edges[cur].erase(input[i]);
+				edges[input[i]].erase(cur);
+			}
+			else ac = false;
+		}
+	}
+	if (ac)cout << "YES" << endl;
+	else cout << "NO" << endl;
+}
+void prob_P(void) {
 
 }
