a01a5cd1d39f2995f35885949587eff89175aad5
ahmedmadbouly186 <66012617+ahmedmadbouly186@users.noreply.github.com>
2023-04-28 20:02:37 +0300

    solve problem d in sheet 4 coach acdemy dijstkra

--- coach_acdemy_shhet4_dijstkra.cpp
+++ coach_acdemy_shhet4_dijstkra.cpp
@@ -19,14 +19,14 @@ typedef vector<vector<int>> matrix;
 #define ff(x, z, y) for (ll x = z; x < y; x++)
 #define yes cout << "YES" << endl
 #define no cout << "NO" << endl
-#define print1(x) cout <<"out: "<< x << endl
+#define print1(x) cout << "out: " << x << endl
 #define print(x) cout << x << endl
 #define is_odd(x) (x % 2 != 0)
-#define all(v) v.begin(),v.end()
+#define all(v) v.begin(), v.end()
 #define sortv(v) sort(all(v))
-vector<vector<pair<int, ll>>>g;
-vector<ll>dist;
-vector<int>par;
+vector<vector<pair<int, ll>>> g;
+vector<ll> dist;
+vector<int> par;
 int n, m, k;
 ////// solution for problem a
 void dijstkra_a(int src);
@@ -41,7 +41,10 @@ void prob_b(void);
 void dijstkra_c();
 void prob_c(void);
 
+////// solution for problem d
+void dijstkra_d(int);
 void prob_d(void);
+
 void prob_e(void);
 void prob_f(void);
 void prob_g(void);
@@ -56,50 +59,55 @@ void prob_N(void);
 void prob_O(void);
 void prob_P(void);
 
-
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	/*freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);*/
-	prob_a();
+	prob_d();
 	return 0;
 }
 
-void dijstkra_a(int src) {
+void dijstkra_a(int src)
+{
 	dist[src] = 0;
-	priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>>pq;
-	pq.push({ 0,src });
-	while (!pq.empty()){
+	priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq;
+	pq.push({0, src});
+	while (!pq.empty())
+	{
 		int node = pq.top().second;
 		ll cur_cost = pq.top().first;
 		pq.pop();
 		if (dist[node] < cur_cost)
 			continue;
-		for (auto& ch : g[node]) {
+		for (auto &ch : g[node])
+		{
 			int next_node = ch.first;
 			ll next_cost = cur_cost + ch.second;
-			if (dist[next_node] > next_cost) {
+			if (dist[next_node] > next_cost)
+			{
 				dist[next_node] = next_cost;
-				pq.push({ next_cost,next_node });
+				pq.push({next_cost, next_node});
 			}
 		}
 	}
 }
-void prob_a(void) {
+void prob_a(void)
+{
 	int t = 1;
 	cin >> t;
-	f(tt, t) {
+	f(tt, t)
+	{
 		int src, dis;
 		cin >> n >> m >> src >> dis;
 		g = vector<vector<pair<int, ll>>>(1 + n);
 		dist = vector<ll>(1 + n, 1e12);
-		f(i, m) {
-			int u, v, w; cin >> u >> v >> w;
-			g[u].push_back({ v,w });
-			g[v].push_back({ u,w });
+		f(i, m)
+		{
+			int u, v, w;
+			cin >> u >> v >> w;
+			g[u].push_back({v, w});
+			g[v].push_back({u, w});
 		}
 		dijstkra_a(src);
 		if (dist[dis] == 1e12)
@@ -109,11 +117,12 @@ void prob_a(void) {
 	}
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void dijstkra_b(int src) {
+void dijstkra_b(int src)
+{
 	dist[src] = 0;
 	par[src] = -1;
-	priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>>pq;
-	pq.push({ 0,src });
+	priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq;
+	pq.push({0, src});
 	while (!pq.empty())
 	{
 		ll current_dist = pq.top().first;
@@ -121,83 +130,103 @@ void dijstkra_b(int src) {
 		pq.pop();
 		if (current_dist > dist[cur_node])
 			continue;
-		for (auto& ch : g[cur_node]) {
+		for (auto &ch : g[cur_node])
+		{
 			int next = ch.first;
 			ll next_dist = current_dist + ch.second;
-			if (next_dist < dist[next]) {
+			if (next_dist < dist[next])
+			{
 				dist[next] = next_dist;
 				par[next] = cur_node;
-				pq.push({ next_dist ,next });
+				pq.push({next_dist, next});
 			}
 		}
 	}
 }
-void path_b(int node) {
+void path_b(int node)
+{
 	if (par[node] != -1)
 		path_b(par[node]);
 	cout << node << " ";
 }
-void prob_b(void) {
+void prob_b(void)
+{
 	int t = 1;
-	//cin >> t;
-	f(tt, t) {
+	// cin >> t;
+	f(tt, t)
+	{
 		cin >> n >> m;
 		g = vector<vector<pair<int, ll>>>(1 + n);
 		dist = vector<ll>(1 + n, 1e17);
 		par = vector<int>(1 + n, 0);
-		f(i, m) {
-			int u, v, w; cin >> u >> v >> w;
-			g[u].push_back({ v,w });
-			g[v].push_back({ u,w });
+		f(i, m)
+		{
+			int u, v, w;
+			cin >> u >> v >> w;
+			g[u].push_back({v, w});
+			g[v].push_back({u, w});
 		}
 		dijstkra_b(1);
-		if (par[n] == 0)print(-1);
+		if (par[n] == 0)
+			print(-1);
 		else
 			path_b(n);
 	}
 }
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void dijstkra_c() {
-	priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>>pq;
-	ff(i, 1, 1 + n) {
-		pq.push({ dist[i],i });
+void dijstkra_c()
+{
+	priority_queue<pair<ll, int>, vector<pair<ll, int>>, greater<>> pq;
+	ff(i, 1, 1 + n)
+	{
+		pq.push({dist[i], i});
 	}
-	while (!pq.empty()) {
+	while (!pq.empty())
+	{
 		ll cur = pq.top().first;
 		int node = pq.top().second;
 		pq.pop();
 		if (dist[node] < cur)
 			continue;
-		for (auto& ch : g[node]) {
+		for (auto &ch : g[node])
+		{
 			int next = ch.first;
 			ll next_len = cur + ch.second;
-			if (next_len < dist[next]) {
+			if (next_len < dist[next])
+			{
 				dist[next] = next_len;
-				pq.push({ dist[next],next });
+				pq.push({dist[next], next});
 			}
 		}
 	}
 }
-void prob_c(void) {
+void prob_c(void)
+{
 	int t = 1;
-	//cin >> t;
-	f(tt, t) {
+	// cin >> t;
+	f(tt, t)
+	{
 		cin >> n >> m;
 		g = vector<vector<pair<int, ll>>>(1 + n);
 		dist = vector<ll>(1 + n);
-		f(i, m) {
-			int u, v; ll w;
+		f(i, m)
+		{
+			int u, v;
+			ll w;
 			cin >> u >> v >> w;
 			w *= 2;
-			g[u].push_back({ v,w });
-			g[v].push_back({ u,w });
+			g[u].push_back({v, w});
+			g[v].push_back({u, w});
 		}
-		f(i, n) {
-			ll a; cin >> a;
+		f(i, n)
+		{
+			ll a;
+			cin >> a;
 			dist[i + 1] = a;
 		}
 		dijstkra_c();
-		ff(i, 1, 1 + n) {
+		ff(i, 1, 1 + n)
+		{
 			cout << dist[i] << " ";
 		}
 		cout << endl;
@@ -205,42 +234,90 @@ void prob_c(void) {
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void prob_d(void) {
-
+void dijstkra_d(int src)
+{
+	dist[src] = 0;
+	priority_queue<pair<ll, int>> pq;
+	pq.push({0, src});
+	while (!pq.empty())
+	{
+		int node = pq.top().second;
+		int cur_cost = abs(pq.top().first);
+		pq.pop();
+		if (dist[node] < cur_cost)
+			continue;
+		for (auto &ch : g[node])
+		{
+			int nex = ch.first;
+			int next_cost = ch.second + cur_cost;
+			if (dist[nex] <= next_cost)
+				continue;
+			dist[nex] = next_cost;
+			pq.push({next_cost * -1ll, nex});
+		}
+	}
 }
-void prob_e(void) {
 
+void prob_d(void)
+{
+	int t;
+	cin >> t;
+	f(tt, t)
+	{
+		int s, d;
+		cin >> n >> m >> s >> d;
+		g = vector<vector<pair<int, ll>>>(1 + n);
+		dist = vector<ll>(1 + n, 1e17);
+		f(i, m)
+		{
+			int u, v, w;
+			cin >> u >> v >> w;
+			g[u].push_back({v, w});
+			g[v].push_back({u, w});
+		}
+		dijstkra_d(s);
+		cout << "Case #" << tt + 1 << ": ";
+		if (dist[d] == 1e17)
+			cout << "unreachable";
+		else
+			cout << dist[d];
+		cout << endl;
+	}
 }
-void prob_f(void) {
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void prob_e(void)
+{
 }
-void prob_g(void) {
-
+void prob_f(void)
+{
 }
-void prob_h(void) {
-
+void prob_g(void)
+{
 }
-void prob_i(void) {
-
+void prob_h(void)
+{
 }
-void prob_j(void) {
-
+void prob_i(void)
+{
 }
-void prob_k(void) {
-
+void prob_j(void)
+{
 }
-void prob_l(void) {
-
+void prob_k(void)
+{
 }
-void prob_M(void) {
-
+void prob_l(void)
+{
 }
-void prob_N(void) {
-
+void prob_M(void)
+{
 }
-void prob_O(void) {
-
+void prob_N(void)
+{
 }
-void prob_P(void) {
-
+void prob_O(void)
+{
+}
+void prob_P(void)
+{
 }
